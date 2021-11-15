27c7c00c020cbc029c943f4d540b771f65d08fef
doncwr <khlzfan03@gmail.com>
2021-11-15 12:13:24 +0700

    first commit

---
+++ Script/splash.js
@@ -0,0 +1,11 @@
+/*const splash = document.querySelector(".splash");
+
+document.addEventListener("DOMContentLoaded", (e) => {
+
+    setTimeout(() => {
+      splash.classList.add("none");
+    }, 4000);
+  }); */
+
+  alert ('Halo, Buddies!');
+  alert ('Selamat datang di website Purrfect Kimbap :)!');
